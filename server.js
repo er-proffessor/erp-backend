c257ad69f2748a698f359b0a03de2f5dd3c47191
Piyush <piyushshrivastava456@gmail.com>
2026-02-10 00:16:14 +0530

    Added env safety checks

--- server.js
+++ server.js
@@ -1,5 +1,9 @@
 require("dotenv").config();
 
+if (!process.env.MONGO_URI || !process.env.JWT_SECRET) {
+  throw new Error("Missing required environment variables");
+}
+
 const app = require("./src/app");
 
 const connectDB = require("./src/config/db");
